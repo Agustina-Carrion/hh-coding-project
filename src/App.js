56c520962935bd3f4d8b57e4c4ad8768a4d90eff
Celine Chan <cctinlone@gmail.com>
2023-04-03 13:26:57 +0200

    chore: header top navbar added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="bg-black w-screen h-screen">
+    <div className="bg-black py-1 w-screen h-screen">
       <Header />
       <h1 className="text-white">Hello</h1>
       <h2 className="text-red-800">Hamburg</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
 const Header = () => {
-return (
-    <div className="text-white">Hello</div>
-)
-}
+  return (
+    <nav className="p-2 max-w-sm mx-auto bg-zinc-300 rounded-full shadow-md flex sm:justify-center space-x-4">
+      {[
+        ["Home", "/dashboard"],
+        ["Photos", "/photos"],
+        ["Projects", "/projects"],
+        ["About", "/about"],
+      ].map(([title, url]) => (
+        <a
+          href={url}
+          className="rounded-lg px-3 py-4 text-zinc-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+        >
+          {title}
+        </a>
+      ))}
+    </nav>
+  );
+};
 export default Header;
