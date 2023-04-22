698439d32e07ec9f7c6206e45d0865f005827fe6
Celine Chan <cctinlone@gmail.com>
2023-04-22 20:59:20 +0200

    feat: add responsive hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+
 import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="bg-black py-1 w-screen h-screen">
+    <div className="py-1 w-screen h-screen">
       <Header />
-      <h1 className="text-white">Hello</h1>
-      <h2 className="text-red-800">Hamburg</h2>
+      {/* <i className="fa-solid fa-burger"></i> */}
     </div>
   );
 };
