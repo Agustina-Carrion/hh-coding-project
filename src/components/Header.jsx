38a984a864e4d08486c2e30a3e7ab1e83eff03fc
Agustina Carrion <agus.carrion@gmail.com>
2023-06-14 19:50:41 +0200

    feat: styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 
   return (
     <>
-      <nav className="p-2 max-w-2xl mx-auto bg-zinc-300 rounded-full shadow-md lg:flex sm:justify-center space-x-4 hidden">
+      <nav className="px-4 max-w-2xl mx-auto bg-white/90 dark:bg-zinc-800/90 rounded-full shadow-md lg:flex sm:justify-center space-x-4 hidden">
         {[
           ["Home", "/"],
           ["Photos", "/photos"],
@@ -15,7 +15,7 @@ export default function Header() {
         ].map(([title, url]) => (
           <Link
             href={url}
-            className="rounded-full px-3 py-4 text-4xl text-zinc-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+            className="rounded-full p-2.5 text-2xl text-zinc-700 dark:text-zinc-200 dark:hover:text-killarney-400 font-medium hover:text-killarney-300"
           >
             {title}
           </Link>
@@ -24,19 +24,20 @@ export default function Header() {
       {/* hamburger icon in mobile view*/}
       <section className="flex justify-end lg:hidden">
         <div
-          className="text-5xl px-8 py-8"
+          className="w-6 h-6 p-2.5"
           onClick={() => setMobileNav((prev) => !prev)}
         >
-          <i className="fa-solid fa-burger"></i>
+          <i className="fa-solid fa-burger dark:text-white/90"></i>
         </div>
         {/* if mobile view, then show the showMenuNav or hide it, these are in the style at the bottom*/}
-        <div className={mobileNav ? "showMenuNav" : "hideMenuNav"}>
+        <div className={mobileNav ? "absolute w-full h-[50vh] top-0 left-0 bg-white/90 z-10 flex flex-col justify-evenly items-center dark:bg-zinc-800/90 " : "hidden"}>
           <div
             className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
             onClick={() => setMobileNav(false)}
           >
             <i className="text-3xl fa-solid fa-arrow-right-from-bracket"></i>
           </div>
+          
           <nav className="flex flex-col items-center justify-between min-h-[250px]">
             {[
               ["Home", "/dashboard"],
@@ -46,7 +47,7 @@ export default function Header() {
             ].map(([title, url]) => (
               <Link
                 href={url}
-                className="rounded-full px-5 py-4 text-4xl text-zinc-700 font-medium hover:bg-slate-200 hover:text-slate-900"
+                className="rounded-full px-5 py-4 text-2xl text-zinc-700 dark:text-zinc-200 dark:active:text-killarney-400 font-medium active:text-killarney-300"
               >
                 {title}
               </Link>
@@ -54,26 +55,6 @@ export default function Header() {
           </nav>
         </div>
       </section>
-      {/* the style can be modified later */}
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 50vh;
-        top: 0;
-        left: 0;
-        background: #8d8b8b;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chestnut: {
+          DEFAULT: "#BC4749",
+          50: "#EDCECE",
+          100: "#E8BFBF",
+          200: "#DDA1A2",
+          300: "#D28384",
+          400: "#C76567",
+          500: "#BC4749",
+          600: "#9C393B",
+          700: "#7B2D2E",
+          800: "#592021",
+          900: "#371415",
+          950: "#260E0E",
+        },
+        killarney: {
+          DEFAULT: "#386641",
+          50: "#B0D4B7",
+          100: "#9FCBA8",
+          200: "#7FB88A",
+          300: "#5EA66C",
+          400: "#4A8756",
+          500: "#386641",
+          600: "#2F5637",
+          700: "#26452C",
+          800: "#1D3522",
+          900: "#142417",
+          950: "#0F1C12",
+        },
+      },
+    },
   },
   plugins: [],
 };
